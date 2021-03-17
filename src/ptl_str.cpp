8ad5ace185db4faac2a24c3cc5751d624190f55d
Julien EMMANUEL <julien.emmanuel@inria.fr>
2021-03-17 16:59:27 +0100

    Stars (style)

--- src/ptl_str.cpp
+++ src/ptl_str.cpp
@@ -2,12 +2,12 @@
  * Author: Julien EMMANUEL
  * Copyright (C) 2019-2021 Bull S.A.S
  * All rights reserved
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License version 2.1 as published by the Free Software Foundation,
  * which comes with this package.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
