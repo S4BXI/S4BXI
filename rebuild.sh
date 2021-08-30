97c49290e0e64c9e3ddfbfc7ee4f8ff8f0dd67b6
Julien EMMANUEL <julien.emmanuel@inria.fr>
2021-08-30 17:19:17 +0200

    More generic build script
    Use an env variable for the global installation root, which will
    easily be reusable in other projects (OMPI, OSU, etc.)

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+S4BXI_INSTALL_ROOT=${S4BXI_INSTALL_ROOT:-"/opt"}
 build_type=${1:-RELEASE}
-install_prefix=${2:-/opt/s4bxi}
+install_prefix=${2:-${S4BXI_INSTALL_ROOT}/s4bxi}
 CORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
 
 # Create build dir
@@ -10,7 +11,7 @@ cd build || exit 1
 rm CMakeCache.txt
 
 # Compile and install
-cmake -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$install_prefix .. && \
+cmake -DSimGrid_SOURCE=~/These/simgrid -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$install_prefix -DSimGrid_PATH="${S4BXI_INSTALL_ROOT}/simgrid" .. && \
 make -j $CORES && \
 make install
 OUTPUT=$?
